9ae391f514ac91dbc650b02fb304c871153bee4c
ori <ori_larusa@hotmail.com>
2022-06-10 14:06:13 -0300

    se realizo la comunicacion entre las propiedades del componente padre hacia el hijo

--- app.js
+++ app.js
@@ -1,3 +1,55 @@
+Vue.component("mostrar_precio",{
+  props:['prop_object'],
+  data(){
+    return{
+      showPrices:false,
+      value:0,
+    } 
+  }, 
+  methods:{
+    toggleShowPrices(){
+      this.showPrices=!this.showPrices
+    }
+  },
+    //funciones computeadas
+    computed:{
+      convertValue(){
+        if(this.value==0){
+          return 0
+        }
+        return this.value/this.prop_object.plata
+      }
+    },
+  
+  template: `
+      <div>
+        <h1 v-bind:class="prop_object.plata > 0 ? 'red':'green' "> {{ prop_object.nom }} </h1>
+
+        <img v-on:mouseover="toggleShowPrices"
+        v-on:mouseout="toggleShowPrices"
+        v-bind:src="prop_object.img" v-bind:alt="prop_object.name">
+
+        <h1>
+          <span v-on:click="toggleShowPrices">
+              {{ prop_object.titulo }}
+              {{ showPrices ? '🙉' : '🙈' }}
+          </span>
+        </h1>
+
+        <input type="number" v-model="value">
+        <span> {{convertValue}}</span>
+
+        <ul v-show="showPrices">
+            <li 
+            v-bind:class="{orange: (prop_object.plata == p.value)}"
+            v-for="(p,i) in prop_object.precios_con_dias"> <p>{{i}}-{{p.dia}}:{{p.value}}</p> </li>
+        </ul>
+      </div>
+`
+        
+} )
+
+
 // primeros pasos en vue
 //1)
 //crear instancia
@@ -5,162 +57,38 @@ new Vue({ // esta estructura se define asi por defecto
   el:'#api', // id=api u otro nombre puede ser
   data(){  // esta estructura se define asi por defecto
     return{
-      titulo:'adios', // invento mi variable que quiero que cambie
-      nom: 'Bitcoin',
-      symbol: 'BTC',
-      color: 'f4f4f4',
-      img: 'https://cryptologos.cc/logos/bitcoin-btc-logo.png',
-      value: 0,
-      plata: 234,
-      precio:[1,2,3],
-      precios_con_dias:[
-        {dia:"Lunes",value:234},
-        {dia:"martes",value:500},
-        
-      ],
-      showPrices:false,
-    }
-  },
-  //puedo crear metodos
-  methods:{
-    toggleShowPrices(){
-      this.showPrices=!this.showPrices
-      this.color=this.color.split('').reverse().join('')
-    }
-  },
-  //funciones computeadas
-  computed:{
-    convertValue(){
-      if(this.value==0){
-        return 0
-      }
-      return this.value/this.plata
-    }
-  },
-})
-
-// OTRO DEL PROFE
-Vue.component('CoinDetail', {
-  props: ['coin'],
-
-  data () {
-    return {
-      showPrices: false,
-      value: 0
-    }
-  },
-
-  methods: {
-    toggleShowPrices () {
-      this.showPrices = !this.showPrices
-
-      this.$emit('change-color', this.showPrices ? 'FF96C8' : '3D3D3D')
-    }
-  },
-
-  computed: {
-    title () {
-      return `${this.coin.name} - ${this.coin.symbol}`
-    },
-
-    convertedValue () {
-      if (!this.value) {
-        return 0
-      }
-
-      return this.value / this.coin.price
-    }
-  },
-
-  created () {
-    console.log('Created CoinDetail...')
-  },
-
-  mounted () {
-    console.log('Mounted CoinDetail...')
-  },
-
-
-  template: `
-    <div>
-      <img
-        v-on:mouseover="toggleShowPrices"
-        v-on:mouseout="toggleShowPrices"
-        v-bind:src="coin.img"
-        v-bind:alt="coin.name"
-      >
-      <h1 v-bind:class="coin.changePercent > 0 ? 'green' : 'red'">
-        {{ title }}
-        <span v-if="coin.changePercent > 0">👍</span>
-        <span v-else-if="coin.changePercent < 0">👎</span>
-        <span v-else>🤞</span>
-
-        <span v-on:click="toggleShowPrices">
-          {{ showPrices ? '🙈' : '🐵' }}
-        </span>
-      </h1>
-
-      <input type="number" v-model="value">
-      <span>{{ convertedValue }}</span>
-
-      <slot name="text"></slot>
-      <slot name="link"></slot>
-
-      <ul v-show="showPrices">
-        <li
-          class="uppercase"
-          v-bind:class="{ orange: p.value === coin.price, red: p.value < coin.price, green: p.value > coin.price }"
-          v-for="(p, i) in coin.pricesWithDays"
-          v-bind:key="p.day">
-          {{ i }} - {{ p.day }} - {{ p.value }}
-        </li>
-      </ul>
-    </div>
-  `
-})
-
-
-// Instanacia de Vue
-new Vue({
-  el: '#app',
-
-  data () {
-    return {
-      btc: {
-        name: 'Bitcoin',
-        symbol: 'BTC',
+      btc:{
+        titulo:'mostrar precios', // invento mi variable que quiero que cambie
+        nom: 'Bitcoin',
         img: 'https://cryptologos.cc/logos/bitcoin-btc-logo.png',
-        changePercent: 0,
-        price: 8400,
-        pricesWithDays: [
-          { day: 'Lunes', value: 8400 },
-          { day: 'Martes', value: 7900 },
-          { day: 'Miercoles', value: 8200 },
-          { day: 'Jueves', value: 9000 },
-          { day: 'Viernes', value: 9400 },
-          { day: 'Sabado', value: 10000 },
-          { day: 'Domingo', value: 10200 },
+        name: 'soy una imagen',
+        plata: 234,
+        precios_con_dias:[
+          {dia:"Lunes",value:234},
+          {dia:"martes",value:500},
+          
         ],
+        color: 'f4f4f4',
+        symbol: 'BTC',
       },
-
-      color: 'f4f4f4',
+      
     }
   },
-
-  created () {
-    console.log('Created...')
-  },
-
-  mounted () {
-    console.log('Mounted...')
-  },
-
-  methods: {
-    updateColor (color) {
-      this.color = color || this.color
-        .split('')
-        .reverse()
-        .join('')
-    }
-  }
+  // //puedo crear metodos
+  // methods:{
+  //   toggleShowPrices(){
+  //     this.showPrices=!this.showPrices
+  //     this.color=this.color.split('').reverse().join('')
+  //   }
+  // },
+  //funciones computeadas
+  // computed:{
+  //   convertValue(){
+  //     if(this.value==0){
+  //       return 0
+  //     }
+  //     return this.value/this.plata
+  //   }
+  // },
 })
+
