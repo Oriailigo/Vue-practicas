5531b15f996203b6064ea46db188a90b3b1ec4a4
ori <ori_larusa@hotmail.com>
2022-12-13 19:16:40 -0300

    se añadio componente hijo al padre

--- app.js
+++ app.js
@@ -1,37 +1,75 @@
-const app=new Vue({
-  el: '#app',
+Vue.component('CoinDetail',{
+  props:['coin'],
   data(){
-    return {name:"Bitcoin",
-            img:"https://cryptologos.cc/logos/bitcoin-btc-logo.png",
-            change_percent:-2,
-            price:8400,
-            value:0,
-            color:"#ff4f",
-            pricesWithDays: [
-              { day: 'Lunes', value: 8400 },
-              { day: 'Martes', value: 7900 },
-              { day: 'Miercoles', value: 8200 },
-              { day: 'Jueves', value: 9000 },
-              { day: 'Viernes', value: 9400 },
-              { day: 'Sabado', value: 10000 },
-              { day: 'Domingo', value: 10200 },
-            ],
-            show_prices:false
-          } 
+    return {showPrices:false,
+      price:8400,
+      value:0,
+    
+    }
   },
   methods:{
-      togle_show_prices(){
-          this.show_prices=!this.show_prices;
-      }
+    toggleShowPrices(){
+        this.showPrices=!this.showPrices;
+    }
   },
   computed:{
-    convertedvalue(){
+    convertValue(){
         if(!this.value){
           return 0;
         }
         return this.value/this.price;
     }
-  }
-}
+  },
+  template:
+    `
+    <div>
+        <h1 v-bind:class="price > 0 ? 'red':'green' "> {{ coin.name }} </h1>   
+        
+        <img v-on:mouseover="toggleShowPrices"
+        v-on:mouseout="toggleShowPrices"
+        v-bind:src="coin.img" v-bind:alt="coin.name">
 
-)
+        <h1>
+          <span v-on:click="toggleShowPrices">
+          mostrar precios
+          {{ showPrices ? '🙉' : '🙈' }}
+          </span>
+        </h1>
+
+        <ul v-show="showPrices">
+          <li 
+          v-bind:class="{orange: (price == p.value)}"
+          v-for="(p,i) in coin.priceswithdays"> <p>{{i}}-{{p.day}}:{{p.value}}</p> </li>
+        </ul>
+
+        <input type="number" v-model="value">
+        <span> {{convertValue}}</span> 
+        
+    </div>
+    `
+  
+})
+
+const app=new Vue({
+  el: '#app',
+  data(){
+    return {
+      btc:{
+        img:"https://cryptologos.cc/logos/bitcoin-btc-logo.png",
+            
+        name:"Bitcoin",
+        priceswithdays: [
+          { day: 'Lunes', value: 8400 },
+          { day: 'Martes', value: 7900 },
+          { day: 'Miercoles', value: 8200 },
+          { day: 'Jueves', value: 9000 },
+          { day: 'Viernes', value: 9400 },
+          { day: 'Sabado', value: 10000 },
+          { day: 'Domingo', value: 10200 },
+        ],
+      }
+     } 
+  },
+
+  
+})
